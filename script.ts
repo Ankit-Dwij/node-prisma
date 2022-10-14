9db23cc4c69309ea1160d662f24f12dd292005bf
Ankit-Dwij <ankitdwiz010@gmail.com>
2022-10-14 14:28:26 +0530

    queries

--- script.ts
+++ script.ts
@@ -2,11 +2,38 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    //prisma client queries here
-    // const user = await prisma.user.create({ data: { name: 'Ankit' } })
-    const user = await prisma.user.deleteMany()
+    // await prisma.user.deleteMany()
+    const user = await prisma.user.create({
+        data: {
+            name: 'Molly',
+            age: 19,
+            email: 'Molly@test4.com',
+            userPreference: { create: { emailUpdates: true } },
+        },
+        include: { userPreference: true },
+    })
     console.log(user)
 }
+
+//FIND
+// async function main() {
+//     const user = await prisma.user.findMany({
+//         where: { name: 'Molly' },
+//         orderBy: { age: 'asc' },
+//         // skip: 1, //pagination
+//         // take: 2,
+//     })
+//     console.log(user)
+// }
+
+//UPDATE
+// async function main() {
+//     const user = await prisma.user.update({
+//         where: { email: 'Molly@test2.com' },
+//         data: { email: 'Molly@updated2.com' },
+//     })
+//     console.log(user)
+// }
 
 main()
     .catch((e) => console.log(e.message))
